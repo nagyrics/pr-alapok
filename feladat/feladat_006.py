21e34dd69193df14ff876201e64c05916f36d891
nagyrics <113088526+nagyrics@users.noreply.github.com>
2023-10-06 13:00:09 +0200

    Add files via upload

---
+++ feladat/feladat_006.py
@@ -0,0 +1,23 @@
+#feladat
+#dk9 108-109 oldal
+"""
+gondolt_szám := 4
+ki: „gondoltam egy számra”
+be: tipp
+tipp átalakítása egésszé
+elágazás
+ha tipp = gondolt_szám:
+ki: dicséret
+elágazás vége
+ki: búcsú
+"""
+
+gondolt_szám = 4
+tipp = input('Gondoltam egy számra. Tippeld meg! ')
+tipp = int(tipp)
+if tipp == gondolt_szám:
+    print(f'Ügyes!')
+    print('Pápá.')
+else:
+    print('nemjo')
+    print('Pápá.')
